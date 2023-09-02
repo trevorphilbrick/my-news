c8f0de433125c5f215241637653e5e9e0222394c
Logan Philbrick <loganphilbrick99@gmail.com>
2023-09-02 17:07:40 -0400

    added avatar and dropdown menu to appbar

--- src/components/feedComponents/NavBar.tsx
+++ src/components/feedComponents/NavBar.tsx
@@ -1,15 +1,66 @@
-import { AppBar, Button, Container, Typography } from "@mui/material";
+import { AppBar, Button, Container, Typography, IconButton, MenuItem, MenuList, ClickAwayListener, Grow, Paper,Popper   } from "@mui/material";
 import { useTheme } from "@mui/material";
+import * as React from 'react';
+
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 type NavbarProps = {
   handleLogout: () => void
 }
 
 function NavBar({ handleLogout }: NavbarProps) {
+  // const [anchorEl, setAnchorEl] = useState(null);
+
   const theme = useTheme();
 
+  // const handleMenu = (event) => {
+  //   setAnchorEl(event.currentTarget);
+  // };
+
+  const handleCloseLogout = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+    handleLogout();
+  };
+
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef(null);
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  function handleListKeyDown(event) {
+    if (event.key === 'Tab') {
+      event.preventDefault();
+      setOpen(false);
+    } else if (event.key === 'Escape') {
+      setOpen(false);
+    }
+  }
+
+  // return focus to the button when we transitioned from !open -> open
+  const prevOpen = React.useRef(open);
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
   return (
-    <AppBar position="static">
+    <AppBar sx={{position: 'static', boxShadow: 'none'}}>
       <Container sx={{display: 'flex', flexDirection: 'row'}}>
         <Typography
           variant="h5"
@@ -19,11 +70,68 @@ function NavBar({ handleLogout }: NavbarProps) {
             fontWeight: 700,
             marginTop: "0.3em",
             marginBottom: "0.3em",
+            mr: 'auto'
           }}
         >
           My News
-        </Typography>
-        <Button variant="text" size="small" sx={{marginLeft: 'auto', color: theme.palette.primary.contrastText}} onClick={handleLogout}>Log Out</Button>
+        </Typography><div style={{zIndex: 1}}>
+        <IconButton size="large" sx={{color: theme.palette.primary.contrastText}} ref={anchorRef}
+          id="composition-button"
+          aria-controls={open ? 'composition-menu' : undefined}
+          aria-expanded={open ? 'true' : undefined}
+          aria-haspopup="true"
+          onClick={handleToggle}><AccountCircleIcon/></IconButton>
+
+<Popper
+          open={open}
+          anchorEl={anchorRef.current}
+          role={undefined}
+          placement="bottom-end"
+          transition
+          disablePortal
+        >
+          {({ TransitionProps, placement }) => (
+            <Grow
+              {...TransitionProps}
+              style={{
+                transformOrigin:
+                  placement === 'bottom-start' ? 'left top' : 'left bottom',
+              }}
+            >
+              <Paper >
+                <ClickAwayListener onClickAway={handleClose}>
+                  <MenuList
+                    autoFocusItem={open}
+                    id="composition-menu"
+                    aria-labelledby="composition-button"
+                    onKeyDown={handleListKeyDown}
+                  >
+                    <MenuItem onClick={handleClose}>Settings</MenuItem>
+                    <MenuItem onClick={handleCloseLogout}>Logout</MenuItem>
+                  </MenuList>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+          )}
+        </Popper></div>
+        {/* <MenuList
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Settings</MenuItem>
+                <MenuItem onClick={handleLogout}>Log out</MenuItem>
+              </MenuList> */}
       </Container>
     </AppBar>
   );
